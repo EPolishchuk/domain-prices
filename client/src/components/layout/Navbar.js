07c2c845ac75b57fe54b5b50fb96b46f8c0ffe74
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2020-07-18 21:56:09 +0300

    Domain Prices | Client: added navbar links and styles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import sm from './styles.modules.scss';
 
 const navbar = () => {
   return (
     <div>
-      <h2>Navbar</h2>
+      <nav className='navbar'>
+        <ol>
+          <li>
+            <h4>Title</h4>
+          </li>
+          <li>
+            <NavLink exact to='/'>
+              Prices
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/domains'>Domains</NavLink>
+          </li>
+          <li>
+            <NavLink to='/registrars'>Registrars</NavLink>
+          </li>
+        </ol>
+      </nav>
     </div>
   );
 };
